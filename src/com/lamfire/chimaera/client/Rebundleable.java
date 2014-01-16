245d2f60b9d1132d31b461cde6ecd7608e889524
lamfire <hayash@163.com>
2014-01-16 18:04:29 +0800

    为KEY绑定添加断线重连重绑功能

---
+++ src/com/lamfire/chimaera/client/Rebundleable.java
@@ -0,0 +1,20 @@
+package com.lamfire.chimaera.client;
+
+import com.lamfire.chimaera.Bundler;
+import com.lamfire.chimaera.OnMessageListener;
+import com.lamfire.chimaera.command.Command;
+import com.lamfire.hydra.net.Session;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lamfire
+ * Date: 14-1-16
+ * Time: 下午3:05
+ * To change this template use File | Settings | File Templates.
+ */
+public interface Rebundleable {
+    public Command getBindCommand(String key,String clientId);
+    public Command getUnbindCommand(String key,String clientId);
+    public Command getPublishCommand(String key,String clientId,byte[] bytes,boolean feedback);
+    public Session rebind(String key,String clientId,OnMessageListener listener);
+}
