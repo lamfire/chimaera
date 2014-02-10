25fe39461ff267420dd353681330efb878864f59
lamfire <hayash@163.com>
2014-02-10 15:24:49 +0800

    增加日志输出

--- src/com/lamfire/chimaera/drainage/ToPollerDrainage.java
+++ src/com/lamfire/chimaera/drainage/ToPollerDrainage.java
@@ -4,6 +4,7 @@ import com.lamfire.chimaera.ChimaeraPoller;
 import com.lamfire.chimaera.ChimaeraSubscribe;
 import com.lamfire.chimaera.OnMessageListener;
 import com.lamfire.chimaera.Poller;
+import com.lamfire.logger.Logger;
 
 /**
  * Created with IntelliJ IDEA.
@@ -13,7 +14,9 @@ import com.lamfire.chimaera.Poller;
  * To change this template use File | Settings | File Templates.
  */
 public class ToPollerDrainage implements OnMessageListener {
+    private static final Logger LOGGER = Logger.getLogger(ToPollerDrainage.class);
     private String toKey;
+    private long received = 0;
 
     public ToPollerDrainage(String toKey){
         this.toKey = toKey;
@@ -22,5 +25,9 @@ public class ToPollerDrainage implements OnMessageListener {
     @Override
     public void onMessage(String key, byte[] message) {
         ChimaeraPoller.getInstance().publish(this.toKey,message);
+        received++;
+        if(received % 10000 == 0){
+            LOGGER.info("["+toKey+"] received messages " + received);
+        }
     }
 }
--- src/com/lamfire/chimaera/drainage/ToSubscribeDrainage.java
+++ src/com/lamfire/chimaera/drainage/ToSubscribeDrainage.java
@@ -2,6 +2,7 @@ package com.lamfire.chimaera.drainage;
 
 import com.lamfire.chimaera.ChimaeraSubscribe;
 import com.lamfire.chimaera.OnMessageListener;
+import com.lamfire.logger.Logger;
 
 /**
  * Created with IntelliJ IDEA.
@@ -11,7 +12,9 @@ import com.lamfire.chimaera.OnMessageListener;
  * To change this template use File | Settings | File Templates.
  */
 public class ToSubscribeDrainage implements OnMessageListener {
+    private static final Logger LOGGER = Logger.getLogger(ToSubscribeDrainage.class);
     private String toKey;
+    private long received = 0;
 
     public ToSubscribeDrainage(String toKey){
         this.toKey = toKey;
@@ -19,5 +22,9 @@ public class ToSubscribeDrainage implements OnMessageListener {
     @Override
     public void onMessage(String key, byte[] message) {
         ChimaeraSubscribe.getInstance().publish(this.toKey,message);
+        received++;
+        if(received % 10000 == 0){
+            LOGGER.info("["+toKey+"] received messages " + received);
+        }
     }
 }
