722de6cbef41113da7f3d53574c7dc2a39e1a1ac
lamfire <hayash@163.com>
2014-02-11 14:37:27 +0800

    更新大批量Queue读写测试

---
+++ test/com/lamfire/chimaera/test/client/queue/MutilQueueReadTest.java
@@ -0,0 +1,34 @@
+package com.lamfire.chimaera.test.client.queue;
+
+import com.lamfire.chimaera.store.FireQueue;
+import com.lamfire.chimaera.test.Config;
+import com.lamfire.utils.Bytes;
+import com.lamfire.utils.RandomUtils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: lamfire
+ * Date: 14-2-11
+ * Time: 下午2:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class MutilQueueReadTest {
+    public static void main(String[] args) {
+        long startAt = System.currentTimeMillis();
+        for(int i=0;i<1000000;i++){
+            FireQueue queue = Config.getFireStore(args).getFireQueue("TEST_QUEUE_"+i);
+            byte[] bytes = queue.pop();
+            if(bytes == null){
+                System.out.println("POP Error");
+            }
+            int val = Bytes.toInt(bytes);
+            if(val != i){
+                System.out.println("Value Error");
+            }
+            if(i % 1000 == 0){
+                System.out.println("[READ] "+i +" -> " +(System.currentTimeMillis() - startAt) +"ms");
+                startAt = System.currentTimeMillis();
+            }
+        }
+    }
+}
